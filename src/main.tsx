9f4fc28c3e376613122630954957dbeb92ba2661
Marjan Alackandy <mrjan283283@gmail.com>
2026-03-02 03:31:01 +0530

    remove BrowserRouter fix routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,7 @@
-
- import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // ✅ ADD THIS
+import { createRoot } from "react-dom/client";
 import App from "./app/App.tsx";
 import "./styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <App />
 );
-  
