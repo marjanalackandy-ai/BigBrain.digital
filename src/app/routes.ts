013f334ee74e8257b131ea9106d52618b4d79f2c
Marjan Alackandy <mrjan283283@gmail.com>
2026-03-02 03:17:03 +0530

    fix routing rendering issue

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter } from "react-router-dom";
 import Layout from "./layout/Layout";
 import Home from "./pages/Home";
 import Services from "./pages/Services";
